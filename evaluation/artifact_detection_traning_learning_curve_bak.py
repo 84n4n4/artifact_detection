07d0f64e343d66f92daab37b68dd7a9e7686d58b
Thomas Hirsch <84n4n4@gmail.com>
2022-02-25 18:05:18 +0100

    smallies, learning curve

--- evaluation/artifact_detection_traning_learning_curve_bak.py
+++ evaluation/artifact_detection_traning_learning_curve_bak.py
@@ -21,10 +21,10 @@ OUT_PATH = root_dir() + 'evaluation/out/learning_curve/'
 def main():
     for lang in LANGUAGES:
         # lang = 'cpp'
-        df = get_learning_curve_data(lang)
-        df = get_learning_curve_data_fractional(lang)
-        # df = pandas.read_csv(OUT_PATH + 'cpp_artifact_detection_summary.csv')
-        # plot_learning_curve(df, lang)
+        # df = get_learning_curve_data(lang)
+        # df = get_learning_curve_data_fractional(lang)
+        df = pandas.read_csv(OUT_PATH + lang + '_artifact_detection_summary.csv')
+        plot_learning_curve(df, lang)
         # scoring_report(df)
 
 
